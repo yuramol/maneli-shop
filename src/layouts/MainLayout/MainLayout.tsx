68992bfc3b6059e427b75b4ddedea529981d1e8a
vitalekp <vitalekpushkarov@gmail.com>
2023-04-19 09:40:42 +0300

    fix MainLayout

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -8,8 +8,8 @@ interface LayoutProps {
 export const MainLayout: FC<LayoutProps> = ({ children }) => (
   <div className="flex flex-col min-h-screen">
     <Header />
-    <main className="flex flex-col flex-auto min-h-full">
-      <div className="flex w-full h-full">{children}</div>
+    <main className="flex flex-col flex-auto min-h-full w-full h-full">
+      {children}
     </main>
     <Footer />
   </div>
