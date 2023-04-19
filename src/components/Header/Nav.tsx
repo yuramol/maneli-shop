61f28250cf949c877910639da3b292390f250386
yuramol <yuramol86@gmail.com>
2023-04-19 13:23:13 +0300

    Fix Drawer

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -43,54 +43,50 @@ export const Nav = ({
       </ul>
     </nav>
   ) : (
-    <div
-      className={
-        'flex fixed top-0 left-0 z-20 w-full h-full transition-all duration-500 transform -translate-x-full  shadow-lg ' +
-        (isBurgerMenuOpen ? 'translate-x-0' : '-translate-x-full')
-      }
-    >
-      <div className="flex justify-between w-[90%] bg-white">
-        <nav className="flex">
-          <ul className="flex flex-col gap-6 px-4 py-20 font-bold text-xl">
-            <li
-              className="cursor-pointer"
-              onClick={() => {
-                scrollToTop();
-                handleMenuClick();
-              }}
-            >
-              Головна
-            </li>
-            <li
-              className="cursor-pointer"
-              onClick={() => {
-                scrollToBottom();
-                handleMenuClick();
-              }}
-            >
-              Контакти
-            </li>
-          </ul>
-        </nav>
-        <button className="mt-7 mr-7 w-6 h-6" onClick={handleMenuClick}>
-          <svg
-            width="26"
-            height="26"
-            viewBox="0 0 26 26"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M25 1L1 25M1 1L25 25"
-              stroke="#333333"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </button>
+    <>
+      <div
+        className={
+          'flex fixed top-0 left-0 z-20 w-[90%] h-full transition-all duration-500 transform -translate-x-full ' +
+          (isBurgerMenuOpen ? 'translate-x-0' : '-translate-x-full')
+        }
+      >
+        <div className="flex w-full justify-between  bg-white">
+          <nav className="flex">
+            <ul className="flex flex-col gap-6 px-4 py-20 font-bold text-xl">
+              <li
+                className="cursor-pointer"
+                onClick={() => {
+                  scrollToTop();
+                  handleMenuClick();
+                }}
+              >
+                Головна
+              </li>
+              <li
+                className="cursor-pointer"
+                onClick={() => {
+                  scrollToBottom();
+                  handleMenuClick();
+                }}
+              >
+                Контакти
+              </li>
+            </ul>
+          </nav>
+          <button className="mt-7 mr-7 w-6 h-6" onClick={handleMenuClick}>
+            <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M25 1L1 25M1 1L25 25"
+                stroke="#333333"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-      <div className="w-[10%] bg-black opacity-40"></div>
-    </div>
+      {isBurgerMenuOpen && <div className="flex fixed top-0 left-0 h-full w-full z-19 bg-black opacity-40" />}
+    </>
   );
 };
