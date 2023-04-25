a447df5f4d52050006d96ada881db67d23f7c932
vitalekp <vitalekpushkarov@gmail.com>
2023-04-25 13:26:55 +0300

    feat: create Catalog page layout

--- src/legos/ProductCard/ProductCard.tsx
+++ src/legos/ProductCard/ProductCard.tsx
@@ -16,7 +16,7 @@ interface Props {
 export const ProductCard: FC<Props> = ({ productInfo }) => {
   const { title, image, discount, price } = productInfo;
   return (
-    <div className="max-w-[156px] sm:max-w-[373px] flex flex-col pt-4 pb-8 sm:py-20">
+    <div className="max-w-[373px]  flex flex-col pt-4 pb-8 sm:py-10">
       <CardImage image={image} discount={discount} />
       <CardBody title={title} price={price} discount={discount} />
     </div>
