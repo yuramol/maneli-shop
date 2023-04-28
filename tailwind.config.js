be644adce8302313ebd8a21892d4976462d8f8b8
vitalekp <vitalekpushkarov@gmail.com>
2023-04-28 08:45:40 +0300

    create legos typography classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,8 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
     },
   },
   plugins: [],
