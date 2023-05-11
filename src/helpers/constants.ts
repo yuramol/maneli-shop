0afa723a8eae941e39c0891c4079494cbdc8d737
dumik <dumik.alive@gmail.com>
2023-05-11 16:11:22 +0300

    Delete constants

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,1 @@
 export const GRAPHQL_API = `${process.env.BASE_URL}/graphql/`;
-export const LG_SCREEN_BREAKPOINT = 1023;
-export const MD_SCREEN_BREAKPOINT = 900;
-export const XS_SCREEN_BREAKPOINT = 600;
-export const LOCAL_STORAGE_CART = 'cart';
