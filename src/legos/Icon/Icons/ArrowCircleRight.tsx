b6088a92db2602fc0e89513a2a4fe849e467a77d
AirFast <airfast.88@gmail.com>
2023-05-11 14:07:20 +0300

    feat: add review section to product page

---
+++ src/legos/Icon/Icons/ArrowCircleRight.tsx
@@ -0,0 +1,22 @@
+import { IconCommonProps } from '../types';
+
+export const ArrowCircleRight = (style: IconCommonProps) => {
+  return (
+    <svg
+      width="42"
+      height="42"
+      viewBox="0 0 42 42"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={style}
+    >
+      <path
+        d="M21 29L29 21M29 21L21 13M29 21H13M41 21C41 32.0457 32.0457 41 21 41C9.9543 41 1 32.0457 1 21C1 9.9543 9.9543 1 21 1C32.0457 1 41 9.9543 41 21Z"
+        stroke="#9D19F1"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,12 +1,22 @@
 import { useRouter } from 'next/router';
 import Image from 'next/legacy/image';
 
+import { ProductOptionCard } from '@/components';
 import { ComponentContainer, MainLayout } from '@/layouts';
-import { DiscountLabel, Rate } from '@/legos';
+import {
+  ArrowCircleLeft,
+  ArrowCircleRight,
+  CalendarDate,
+  CreditCardShield,
+  DiscountLabel,
+  Rate,
+  Scales,
+  ShieldTick,
+} from '@/legos';
 
 import productImage21 from '../../assets/rectangle-21.png';
 import productImage from '../../assets/rectangle-25.png';
-import { ProductOptionCard } from '@/components';
+import review from '../../assets/review.png';
 
 export default function Product() {
   const { query } = useRouter();
@@ -16,13 +26,10 @@ export default function Product() {
       <ComponentContainer>
         <section className="relative grid md:grid-cols-2 gap-11 items-center mt-4 md:mt-20 before:w-[400px] before:h-[400px] before:absolute before:-top-20 before:-left-44 before:bg-radial-gradient-purple before:opacity-10 before:-z-10 after:w-[400px] after:h-[400px] after:absolute after:-bottom-20 after:-right-44 after:bg-radial-gradient-purple after:opacity-10 after:-z-10">
           <div className="flex flex-col gap-4 md:gap-8">
-            <h1 className="font-bold text-2xl md:text-5xl">
-              Портативна світлодіодна USB лампа
-            </h1>
+            <h1 className="font-bold text-2xl md:text-5xl">Портативна світлодіодна USB лампа</h1>
             <p className="text-sm md:text-lg">
-              Зручна портативна світлодіодна лампа USB. Підійде для походів,
-              кемпінгу, наметів, подорожей, роботи з блокнотом. Живлення
-              здійснюється від power bank.
+              Зручна портативна світлодіодна лампа USB. Підійде для походів, кемпінгу, наметів,
+              подорожей, роботи з блокнотом. Живлення здійснюється від power bank.
             </p>
             <div className="flex relative md:hidden">
               <div className="absolute right-2 sm:right-6 top-2 sm:top-6 z-10">
@@ -69,7 +76,7 @@ export default function Product() {
         </section>
 
         <section className="grid md:grid-cols-2 gap-8 mt-8 md:gap-11 md:mt-20">
-          <div className="rounded-2xl p-8 bg-[#F4F3FD]">
+          <div className="rounded-2xl p-6 sm:p-8 bg-[#F4F3FD]">
             <h2 className="font-bold text-2xl md:text-5xl">Докладний опис</h2>
             <dl className="mt-4 sm:mt-7">
               <span className="flex flex-row gap-1 text-sm sm:text-lg mb-2 sm:mb-6">
@@ -101,9 +108,7 @@ export default function Product() {
                 <dd>50000H</dd>
               </span>
               <span className="flex flex-row gap-1 text-sm sm:text-lg mb-2 sm:mb-6">
-                <dt className="font-semibold">
-                  Підходить для всіх USB-розємів
-                </dt>
+                <dt className="font-semibold">Підходить для всіх USB-розємів</dt>
               </span>
               <span className="flex flex-row gap-1 text-sm sm:text-lg">
                 <dt className="font-semibold">Постачання живлення від USB</dt>
@@ -118,9 +123,7 @@ export default function Product() {
         </button>
 
         <section className="mt-8 md:mt-12">
-          <h2 className="font-bold text-2xl md:text-5xl">
-            Варіанти користування
-          </h2>
+          <h2 className="font-bold text-2xl md:text-5xl">Варіанти користування</h2>
           <div className="grid md:grid-cols-2 gap-8 mt-8 md:gap-11 md:mt-10">
             <ProductOptionCard
               title="Вимкнення світла"
@@ -146,6 +149,47 @@ export default function Product() {
             />
           </div>
         </section>
+
+        <section className="mt-8 md:mt-12">
+          <div className="flex flex-row gap-6 justify-between items-center">
+            <h2 className="font-bold text-2xl md:text-5xl">Відгуки</h2>
+            <div className="flex flex-row gap-6 md:gap-10">
+              <button>
+                <ArrowCircleLeft />
+              </button>
+              <button>
+                <ArrowCircleRight />
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-center mt-8 md:mt-16">
+            <div className="flex sm:w-2/4">
+              <Image src={review} alt="Review photo" />
+            </div>
+          </div>
+          <div className="flex flex-row flex-wrap gap-4 sm:gap-10 justify-center mt-8 md:mt-20">
+            <div className="flex flex-col items-center gap-3 w-40 text-center rounded-2xl p-8 bg-[#F4F3FD]">
+              <CalendarDate />
+              <p className="font-semibold">Доставка 1-3 дні</p>
+            </div>
+            <div className="flex flex-col items-center gap-3 w-40 text-center rounded-2xl p-8 bg-[#F4F3FD]">
+              <CreditCardShield />
+              <p className="font-semibold">Оплата при отримані</p>
+            </div>
+            <div className="flex flex-col items-center gap-3 w-40 text-center rounded-2xl p-8 bg-[#F4F3FD]">
+              <Scales />
+              <p className="font-semibold">Вигідна ціна</p>
+            </div>
+            <div className="flex flex-col items-center gap-3 w-40 text-center rounded-2xl p-8 bg-[#F4F3FD]">
+              <ShieldTick />
+              <p className="font-semibold">Гарантія якості</p>
+            </div>
+          </div>
+        </section>
+
+        <button className="flex justify-center items-center rounded-full bg-[#7613B5] text-white text-base font-semibold h-16 w-full mx-auto my-8 md:my-20 md:w-80">
+          Замовити зараз
+        </button>
       </ComponentContainer>
     </MainLayout>
   );
