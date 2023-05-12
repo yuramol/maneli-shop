1bc56b4efd6170cf792b08c7c23906c79d1c61a8
yuramol <yuramol86@gmail.com>
2023-05-12 11:36:18 +0300

    Fix nextauth url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     BASE_URL: 'https://strapi.maneli.softbee.io/graphql',
-    NEXTAUTH_URL: 'https://strapi.maneli.softbee.io/graphql/api/auth',
+    NEXTAUTH_URL: 'https://strapi.maneli.softbee.io/api/auth',
     NEXTAUTH_SECRET: 'Wq+i1/gD+i1LjQG20a8XgHOZSBSsJXsJ52DSFymliD8=',
   },
 };
