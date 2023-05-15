2e9b1a456d2b6698212e0976061fa852adb7c586
AirFast <airfast.88@gmail.com>
2023-05-15 15:23:49 +0300

    feat: add order form

--- src/legos/ProductCard/layout/CardImage.tsx
+++ src/legos/ProductCard/layout/CardImage.tsx
@@ -4,21 +4,13 @@ import Link from 'next/link';
 
 import { DiscountLabel } from '@/legos/DiscountLabel';
 
-export const CardImage = ({
-  image,
-  discount,
-}: {
-  image: StaticImageData;
-  discount: number;
-}) => (
+export const CardImage = ({ image, discount }: { image: StaticImageData; discount: number }) => (
   <Link href="#">
     <div
       className="relative w-full  h-[156px] sm:h-[373px] bg-white"
       style={{ margin: '0 auto', borderRadius: '15px' }}
     >
-      <div className="absolute right-2 sm:right-6 top-2 sm:top-6 z-10">
-        <DiscountLabel discount={discount} />
-      </div>
+      <DiscountLabel discount={discount} />
       <Image
         src={image}
         alt={'preview-photo'}
