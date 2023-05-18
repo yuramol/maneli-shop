8891b6c55b790cc04bad60af9ca17aef07900330
yuramol <yuramol86@gmail.com>
2023-05-18 17:05:07 +0300

    Fix styles

--- src/components/AddImage/components/ButtonAddBanner.tsx
+++ src/components/AddImage/components/ButtonAddBanner.tsx
@@ -59,7 +59,7 @@ export const ButtonAddBanner: FC<Props> = ({ loadingSrc, handleUploadImg }) => {
       ) : (
         <label
           for="dropzone-file"
-          class="flex flex-col items-center justify-center w-full h-64 border-2 border-[#7613B5] border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+          class="flex flex-col items-center justify-center w-full h-full border-2 border-[#7613B5] border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
         >
           <div class="flex flex-col items-center justify-center pt-5 pb-6">
             <button
